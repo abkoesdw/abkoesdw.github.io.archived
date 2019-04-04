391e5b5c72d297424328dbc8f42296bc06e6265a
mzyskk <arief.koesdwiady@gm.com>
2019-04-04 16:07:48 -0400

    added publications

--- assets/js/arief.js
+++ assets/js/arief.js
@@ -22,3 +22,19 @@ function w3_close() {
     mySidebar.style.display = "none";
     overlayBg.style.display = "none";
 }
+
+function myButton() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
